f08187602bdb125c3f154ef0e4b290d83b8a7924
souravsathyan <souravksathyan@gmail.com>
2025-08-14 22:41:29 +0530

    finished hero section

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+      <div className="h-dvh"></div>
     </main>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,83 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars, words" });
+    const paraSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((chars) => chars.classList.add("text-gradient"));
+
+    gsap.from(heroSplit.chars, {
+      yPercent: 80,
+      stagger: 0.04,
+      duration: 1.2,
+      ease: "expo.out",
+    });
+
+    gsap.from(paraSplit.lines, {
+      yPercent: 100,
+      stagger: 0.06,
+      duration: 1.8,
+      ease: "expo.out",
+      delay: 0.5,
+      opacity: 0,
+    });
+
+    gsap.from(".demo", {
+      xPercent: -60,
+      //   stagger: 0.06,
+      duration: 0.8,
+      ease: "power1.inOut",
+      delay: 0.5,
+      opacity: 0,
+    });
+
+    gsap.from(".cock-link", {
+      x: 200,
+      duration: 0.8,
+      ease: "power1.inOut",
+      delay: 2,
+      opacity: 0,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 300 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
+  }, []);
   return (
     <>
       <section id="hero" className="noisy">
@@ -20,7 +97,7 @@ const Hero = () => {
         <div className="body ">
           <div className="content ">
             <div className="space-y-5 hidden md:block">
-              <p>Crisp. Cool. Classic</p>
+              <p className="demo">Crisp. Cool. Classic</p>
               <p className="subtitle">
                 Sip the Spirit <br /> of Summer
               </p>
@@ -32,11 +109,22 @@ const Hero = () => {
                 creative flair, and timeless recipes - designed to delight your
                 senses.
               </p>
-              <a href="#cocktails">View Cocktails</a>
+              <a className="cock-link" href="#cocktails">
+                View Cocktails
+              </a>
             </div>
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0 ">
+        <video
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+          ref={videoRef}
+        />
+      </div>
     </>
   );
 };
